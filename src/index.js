d7dc00b506068f27ec277412981f7477aaded842
liul85 <liul85@gmail.com>
2017-05-09 20:34:38 +0800

    fetch videos at the first step

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const YOUTUBE_API_KEY = 'AIzaSyDKFPa8BJUcOsLHihCBCdfoXGJlObRIfU8'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-    )
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {videos: []}
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+      )
+  }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'))
