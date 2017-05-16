90ccdf8619a2796a40b3d812f6032e75bdfbb88c
liul85 <liul85@gmail.com>
2017-05-16 19:26:51 +0800

    add click event to the video list item

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -3,7 +3,7 @@ import VideoItem from './video-list-item'
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoItem key={video.etag} video={video} />
+    return <VideoItem key={video.etag} video={video} onVideoSelect={props.onVideoSelect}/>
   })
   return (
     <ul className="col-md-4 list-group">
